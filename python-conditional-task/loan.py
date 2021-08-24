3d060e9d39834cdbb94d763fd396cb5e3547f755
Aaliyah Jardien <aaliyahjar13@gmail.com>
2021-08-24 11:07:25 +0200

    update

---
+++ python-conditional-task/loan.py
@@ -0,0 +1,16 @@
+income = float(input("Enter your income per year in $:"))
+
+if income > 3000:
+    work_period = int(input("How long have you been working"))
+    if work_period >= 2:
+        print("You qualify for a loan.")
+    else:
+        print("You do not qualify.")
+ print('Still inside the first if for 30000')
+else:
+
+
+if score >= 90:
+    print("Your grade is A.")
+    if score < 90
+
